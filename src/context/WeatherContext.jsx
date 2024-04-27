6bb3edb22bc25237413c819c0480adb201f47c45
Pamood <pamoodjayaratne2001@gmail.com>
2024-04-28 00:17:57 +0530

    Add react-query dependency and implement geolocation in WeatherContext.jsx

--- src/context/WeatherContext.jsx
+++ src/context/WeatherContext.jsx
@@ -1,16 +1,20 @@
 import { createContext, useState, useEffect } from "react"
 import getFormattedWeatherData from "../api/weatherApi"
+import { toast } from "react-toastify"
 
 export const WeatherContext = createContext()
 
 export const WeatherProvider = ({ children }) => {
-  const [query, setQuery] = useState({ q: "colombo" })
+  const [query, setQuery] = useState({})
   const [units, setUnits] = useState("metric")
   const [weather, setWeather] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     const fetchWeatherData = async () => {
+      if (!query.q && !query.lat && !query.lon) {
+        return
+      }
       setIsLoading(true)
       try {
         const data = await getFormattedWeatherData({ ...query, units })
@@ -20,8 +24,30 @@ export const WeatherProvider = ({ children }) => {
       }
       setIsLoading(false)
     }
+
     fetchWeatherData()
   }, [query, units])
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setQuery({
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+          })
+        },
+        (error) => {
+          if (error.code === error.PERMISSION_DENIED) {
+            toast.error("🚫 Permission for geolocation was denied.")
+          }
+          console.error(error)
+        }
+      )
+    } else {
+      toast.error("🚫 Geolocation is not supported by this browser.")
+    }
+  }, [])
 
   return (
     <WeatherContext.Provider
